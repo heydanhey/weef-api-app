fa4ddaadba40cfc3ca84f32f9df6f74ffb8b6232
heydanhey <danhey@gmail.com>
2016-02-23 20:39:31 -0600

    Added API Security

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214154330) do
+ActiveRecord::Schema.define(version: 20160224022103) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160214154330) do
     t.integer  "emotion_id", limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      limit: 255
+    t.string   "api_key",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
